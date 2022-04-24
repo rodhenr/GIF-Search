18568725a1c02980a56e09332f49061a5af657a8
Rodrigo Henrique <rodhenrc@outlook.com>
2022-04-24 14:51:22 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>PROCURE POR UM GIF</h1>
+      <input type="text" name="" id="" />
+      <button>PROCURAR</button>
+    </div>
+  );
+}
+
+export default App;
