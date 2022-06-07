5fb9bfefd7b7f59e37e26ae6f0b894ecee8a2823
Rodrigo Henrique <rodhenrc@outlook.com>
2022-06-07 16:03:09 -0300

    Pequena mudança no teste para detectar se não está exibindo gifs no estado inicial da aplicação

--- cypress/e2e/Gifs.spec.cy.js
+++ cypress/e2e/Gifs.spec.cy.js
@@ -1,6 +1,9 @@
-describe("finding gifs", () => {
+describe("procurando por gifs", () => {
   it("deve renderizar os gifs de acordo com o termo digitado ou exibir os itens populares quando clicado em POPULAR. Também faz a checagem da informação exibida de acordo com o nome do termo pesquisado e quantidade de itens exibidos", () => {
     cy.visit("/");
+    cy.get('[data-cy="no-gifs"]')
+      .should("have.text", "Nenhum item para exibir")
+      .and("be.visible");
     cy.get('[data-cy="input"]').type("react");
     cy.checkGifs("search");
     cy.checkInfo("searched", "qtd", "REACT", "20");
@@ -10,6 +13,9 @@ describe("finding gifs", () => {
       "https://giphy.com/search/react"
     );
     cy.get('[data-cy="home"]').click();
+    cy.get('[data-cy="no-gifs"]')
+      .should("have.text", "Nenhum item para exibir")
+      .and("be.visible");
     cy.checkGifs("popular");
     cy.checkInfo("searched", "qtd", "POPULAR", "20");
   });
